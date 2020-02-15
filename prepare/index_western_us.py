e2c404843487503c939efa8b523c65c8fcac7b61
tonybutzer <tonybutzer@gmail.com>
2020-02-15 23:19:00 +0000

    automatic git update from Makefile

--- prepare/index_western_us.py
+++ prepare/index_western_us.py
@@ -1,13 +1,20 @@
-#from index_lib import index_records
+from index_lib import index_records
+import timeit
 
 def cli():
     print ("Hello Tony")
 
-    for path in range (40,41):
-        for row in range (30,33):
+    start = timeit.timeit()
+    for path in range (31,46):
+        for row in range (26,38):
             for year in range(2013,2019+1):
                 print(path, row, year)
+                index_records(path,row,year)
+    end = timeit.timeit()
+    print("TIMED = ", end - start)
 
 
 if __name__ == '__main__':
     cli()
+
+
